92fbb73c34aa45474c38c2c3056b0cb544c1454e
shaibujnr <s.shaibu.jnr@gmail.com>
2016-10-19 20:41:20 +0000

    created DatabaseHelper class in helpers package

---
+++ app/src/main/java/ng/kedco/gridix/models/BaseEntity.java
@@ -0,0 +1,17 @@
+package ng.kedco.gridix.models;
+
+/**
+ * Created by shaibujnr on 10/19/16.
+ */
+
+public abstract class BaseEntity {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
